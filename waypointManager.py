0e3f235c651c4252f246c9a8409078bb5ade57e6
Armin Straller <armin@Armins-MacBook-Pro-5.local>
2020-12-11 16:54:33 +0100

    Added 'LIN' Waypoint Type

--- waypointManager.py
+++ waypointManager.py
@@ -9,7 +9,7 @@ class WaypointManager(object):
         self.updateInterval = uI
         self.waypoints = []
         self.aWI = 0    #activeWaypointIndex
-        self.LINNumDistance = 0.01 # 1cm
+        self.LINNumDistance = 0.001 # 1cm
         self.robotArm = RobotArm(self.updateInterval)
         self.robotArm.updateVisu(self.robotArm.q)
         self.addWaypoint('PTP', 0.5, 0, 0, 0, 0.1)
@@ -55,31 +55,46 @@ class WaypointManager(object):
         
         #LIN Mode will intrapolate other Waypoints every cm between the last waypoint and the target waypoint
         if type == 'LIN':
-            if len(self.waypoints) > 0:
-                waypoint.distanceToNext = self.getDistanceTwoPoints(waypoint, self.waypoints[len(self.waypoints) - 1])
-
-                # find out the linear equation between start and end point
-                gradient = np.zeros((3,1))
-                gradient[0] = waypoint.x / self.waypoints[self.aWI].x
-                gradient[1] = waypoint.y / self.waypoints[self.aWI].y
-                gradient[2] = waypoint.z / self.waypoints[self.aWI].z
-
-                offset = np.zeros((3,1))
-                gradient[0] = waypoint.x / self.waypoints[self.aWI].x
-                gradient[1] = waypoint.y / self.waypoints[self.aWI].y
-                gradient[2] = waypoint.z / self.waypoints[self.aWI].z
-
+            length = len(self.waypoints)
+            if length > 0:
+                length = length - 1 
+                waypoint.distanceToNext = self.LINNumDistance
                 
-                for i in range(numIntrapolations):
 
+                # find out the transformation Vector between start and end point
+                transVector = self.getVectorBetweenWaypoints(waypoint, self.waypoints[length])
+                norm = self.getDistanceTwoPoints(waypoint, self.waypoints[length])
+                transVectorNorm = (transVector / norm)
 
-
-
-
+                for i in range( int(norm / waypoint.distanceToNext) + 1 ):
+                    offset =  transVectorNorm * self.LINNumDistance * i
+                    id = str(uuid.uuid1())
+                    waypointNum = WayPoint('PTP', self.waypoints[length].x + np.asscalar(offset[0]), self.waypoints[length].y + np.asscalar(offset[1]), self.waypoints[length].z + np.asscalar(offset[2]), waypoint.beta, waypoint.lDP, 'created', id)
+                    waypointNum.distanceToNext = self.LINNumDistance
+                    self.waypoints.append(waypointNum)
+                waypoint.lDP = 10
                 self.waypoints.append(waypoint)
+
+                # set the last waypoint's lDP to a workable Value 
+                self.waypoints[length].lDP = 0.1
             else:
                 raise ValueError('A LIN type Waypoint can not be the first Waypoint!')
             
+    def getVectorBetweenWaypoints(self, w1, w2):
+        transVector = np.zeros(( 3,1 ))
+        
+        waypoint1 = np.zeros(( 3,1 ))
+        waypoint1[0] = w1.x
+        waypoint1[1] = w1.y
+        waypoint1[2] = w1.z
+
+        waypoint2 = np.zeros(( 3,1 ))
+        waypoint2[0] = w2.x
+        waypoint2[1] = w2.y
+        waypoint2[2] = w2.z
+
+        transVector = waypoint1 - waypoint2
+        return transVector
 
 
 @dataclass
